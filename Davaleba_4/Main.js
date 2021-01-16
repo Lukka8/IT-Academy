3ac93bf3dc8cf8ae7158be75a44c656da3692dfe
lukka8_8 <45329766+Lukka8@users.noreply.github.com>
2021-01-16 06:27:20 -0800

    Add files via upload

--- Davaleba_4/Main.js
+++ Davaleba_4/Main.js
@@ -9,6 +9,7 @@ const Backwards = document.getElementById("Backwards")
 const Forward = document.getElementById("Forward")
 const Selectors = document.getElementById("Selectors")
 const Images = document.querySelectorAll('.images img')
+const Buttons = []
 
 var Position = 1
 var ImageCount = Images.length
@@ -19,6 +20,10 @@ var Count = 0
 
 
 const Change = (Number, Type) => {
+
+  ActiveButton = Buttons[Position]
+  Buttons[Position].classList.add("active")
+  
   if (Type) {
     if (ActiveImage) {
       ActiveImage.classList.remove("active")
@@ -44,6 +49,10 @@ const Change = (Number, Type) => {
 
     ActiveImage = Images[Position]
   }
+
+  ActiveButton.classList.remove("active")
+  Buttons[Position].classList.add("active")
+  ActiveButton = Buttons[Position]
 }
 
 Forward.addEventListener("click", () => {
@@ -70,9 +79,11 @@ Images.forEach(Image => {
   Button.dataset.id = Count
 
   Selectors.appendChild(Button)
-
+  Buttons[Count] = Button
   Count++
 });
+
+Buttons.reverse()
 
 
 Change(Position, false)
